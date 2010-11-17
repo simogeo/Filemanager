cf6941d76f31d860cb6f40266d9a560b7f83f507
Simon <simon@linea21.com>
2010-11-17 20:01:33 +0100

    + UTF-8 support for special chars

--- scripts/jquery.filetree/connectors/jqueryFileTree.php
+++ scripts/jquery.filetree/connectors/jqueryFileTree.php
@@ -33,14 +33,14 @@ if(auth()) {
       // All dirs
       foreach( $files as $file ) {
         if( file_exists($path . $file) && !in_array($file, $config['unallowed_dirs']) && $file != '.' && $file != '..' && is_dir($root . $_POST['dir'] . $file) ) {
-          echo "<li class=\"directory collapsed\"><a href=\"#\" rel=\"" . htmlentities($_POST['dir'] . $file) . "/\">" . htmlentities($file) . "</a></li>";
+          echo "<li class=\"directory collapsed\"><a href=\"#\" rel=\"" . htmlentities($_POST['dir'] . $file, ENT_COMPAT, 'UTF-8') . "/\">" . htmlentities($file, ENT_COMPAT, 'UTF-8') . "</a></li>";
         }
       }
       // All files
       foreach( $files as $file ) {
         if( file_exists($path . $file) && !in_array($file, $config['unallowed_files']) && $file != '.' && $file != '..' && !is_dir($root . $_POST['dir'] . $file) ) {
           $ext = preg_replace('/^.*\./', '', $file);
-          echo "<li class=\"file ext_".strtolower($ext)."\"><a href=\"#\" rel=\"" . htmlentities($_POST['dir'] . $file) . "\">" . htmlentities($file) . "</a></li>";
+          echo "<li class=\"file ext_".strtolower($ext)."\"><a href=\"#\" rel=\"" . htmlentities($_POST['dir'] . $file, ENT_COMPAT, 'UTF-8') . "\">" . htmlentities($file, ENT_COMPAT, 'UTF-8'). "</a></li>";
         }
       }
       echo "</ul>";
