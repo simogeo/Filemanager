c7925811481a9d0b9429427281434dcf5b4b9672
Simon <simon@linea21.com>
2011-02-11 09:37:04 +0100

    Russian translation, thanks to Constantine Zykov

---
+++ scripts/languages/ru.js
@@ -0,0 +1,63 @@
+/**
+ * script: ru.js
+ *
+ * description: Russian locale for filemanager.
+ *
+ * author: Constantine Zykov <c.zykov@gmail.com>
+ *
+ */
+{
+    "AUTHORIZATION_REQUIRED": "У вас не хватает прав для работы с менеджером файлов.", 
+    "INVALID_ACTION": "Неверное действие.",
+    "MODE_ERROR": "Ошибка режима.",
+	"DIRECTORY_ALREADY_EXISTS": "Папка '%s' уже существует.",
+	"FILE_ALREADY_EXISTS": "Файл '%s' уже существует.",
+	"UNABLE_TO_CREATE_DIRECTORY": "Не могу создать папку %s.",
+	"INVALID_VAR": "Неверная переменная %s.",
+	"DIRECTORY_NOT_EXIST": "Папка %s не существует.",
+	"UNABLE_TO_OPEN_DIRECTORY": "Не могу открыть папку %s.",
+	"ERROR_RENAMING_DIRECTORY": "Ошибка при переименовывании папки %s в %s.",
+	"ERROR_RENAMING_FILE": "Ошибка при переименовывании файла %s в %s.",
+	"INVALID_DIRECTORY_OR_FILE": "Неверная папка или файл.",
+	"INVALID_FILE_UPLOAD": "Загружен некоррекнтый файл.",
+	"UPLOAD_FILES_SMALLER_THAN": "Пожалуйста, загружайте файлы не больше %s.",
+	"UPLOAD_IMAGES_ONLY": "Пожалуйста, загружайте только изображения, другие типы файлов не поддерживаются.",
+	"UPLOAD_IMAGES_TYPE_JPEG_GIF_PNG": "Пожалуйста, загружайте только изображения типа JPEG, GIF или PNG.",
+	"FILE_DOES_NOT_EXIST": "Файл %s не существует.",
+	"LANGUAGE_FILE_NOT_FOUND": "Файл с настройками для языка не найден.",
+	"default_foldername": "Моя папка",
+	"prompt_foldername": "Введите имя новой папки",
+	"no_foldername": "Не введено имя папки.",
+	"create_folder": "Создать папку",
+	"cancel": "Отмена",
+	"select_from_left": "Выберите элемент слева.",
+	"fck_select_integration": "Футнкция 'Выбрать' используется только для интеграции с FCKEditor.",
+	"new_filename": "Введите новое имя файла",
+	"successful_rename": "Переименовывание успешно.",
+	"rename": "Переименовать",
+	"confirmation_delete": "Вы уверены, что хотите удалить этот файл?",
+	"successful_delete": "Удаление прошло успешно.",
+	"successful_added_file": "Новый файл добавлен успешно.",
+	"successful_added_folder": "Новая папка добавлена успешно.",
+	"select": "Выбрать",
+	"download": "Скачать",
+	"del": "Удалить",
+	"parentfolder": "На уровень выше",
+	"dimensions": "Размеры",
+	"created": "Создан",
+	"modified": "Изменён",
+	"size": "Размер",
+	"name": "Имя",
+	"could_not_retrieve_folder": "Не могу получить содержимое папки.",
+	"yes": "Да",
+	"no": "Нет",
+	"upload": "Загрузить",
+	"new_folder": "Новая папка",
+	"grid_view": "Переключиться в режим сетки",
+	"list_view": "Переключиться в режим списка",
+	"current_folder": "Текущая папка: ",
+	"bytes": " байт",
+	"kb": "Кб",
+	"mb": "Мб",
+	"gb": "Гб"
+}
